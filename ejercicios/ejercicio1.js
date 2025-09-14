c8e6aac844882129d5335a9a5825d337e4a6a8d6
Albany Luciani <albanyluciani@gmail.com>
2025-09-14 15:35:01 -0500

    add initial HTML structure and JavaScript functionality; update user info in JSON

--- ejercicios/ejercicio1.js
+++ ejercicios/ejercicio1.js
@@ -4,3 +4,6 @@
 // Título de la página usando document.title
 // URL actual usando document.URL
 // Mostrar esta información en un div en la página
+
+console.log(document.title); // Título de la página
+console.log(document.URL); // URL actual
